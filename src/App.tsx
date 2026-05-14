b30efad8d27ed270f4898fc5bebf4e82f65d4a30
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-05-14 20:59:50 +0600

    added shadcn and react router
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>
+            <Button>Click me</Button>
+          </h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
