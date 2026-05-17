e89508d503fa19972a77b0ee21051e84fd5d44bb
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-05-17 22:02:39 +0600

    added dark and light theme
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router";
 import "./index.css";
+import { ThemeProvider } from "./providers/theme-provider.tsx";
 import { store } from "./redux/store.ts";
 import { router } from "./routes/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReduxProvider store={store}>
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </ReduxProvider>
   </StrictMode>,
 );
